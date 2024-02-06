495c905de93de302b623c17b006584e9cd747832
camille.minaudo <camille.minaudo@ub.edu>
2024-02-06 11:02:29 +0100

    updated list of packages

--- scripts/000_install_dependencies.R
+++ scripts/000_install_dependencies.R
@@ -1,22 +1,16 @@
 
 
 
-# ---- packages ----
-# install.packages(c('tidyverse','readxl','lubridate','zoo','ggplot2','grid','egg','dplyr','purrr','msm'))
 
-if (!require("remotes", quietly = TRUE))
-  install.packages("remotes")
+if (!require("pacman", quietly = TRUE))
+  install.packages("pacman")
+
+# Then load packages. Pacman will check whether each package has been installed, and if not, will install it automatically.
+
+pacman::p_load("data.table", "dplyr",  "egg", "ggnewscale", "ggplot2", "graphics", "grDevices", "grid", "gridExtra",
+               "lubridate", "minpack.lm", "msm", "pbapply", "plyr", "purrr", "readxl", "remotes","rjson", "rlist", "SimDesign",
+               "stats", "tibble", "tidyr", "utils")
 
 remotes::install_github("Qepanna/GoFluxYourself")
 
 
-
-data.table, dplyr, ggnewscale, ggplot2, graphics, grDevices, grid, gridExtra, egg,
-  lubridate, minpack.lm, msm, pbapply, plyr, purrr, readxl, rjson, rlist, SimDesign,
-  stats, tibble, tidyr, utils)
-
-
-install.packages("pacman")
-# Then load packages. Pacman will check whether each package has been installed, and if not, will install it automatically.
-
-pacman::p_load("stockPortfolio","quadprog")
