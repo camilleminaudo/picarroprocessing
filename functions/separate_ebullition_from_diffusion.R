7daad8bd2027d6293693c77e7f3c475f90b54179
camille.minaudo <camille.minaudo@ub.edu>
2024-02-08 17:18:40 +0100

    improved plot

--- functions/separate_ebullition_from_diffusion.R
+++ functions/separate_ebullition_from_diffusion.R
@@ -10,50 +10,64 @@
 # allowing to distinguish the diffusive flux from the ebullitive flux
 
 
-source('C:/Projects/myGit/my_r_tools/scripts/get_dxdy.R')
-
-
-separate_ebullition_from_diffusion <- function(my_incub){
+separate_ebullition_from_diffusion <- function(my_incub, UniqueID){
   mych4 <- data.frame(time = as.numeric(my_incub$POSIX.time-my_incub$POSIX.time[1]),
                       ch4 = my_incub$CH4dry_ppb)
   mych4$ch4smooth <- smooth.spline(x = mych4$time, y = mych4$ch4, nknots = round(max(mych4$time)/3), spar = 0.6)$y
-
+  
   # compute first derivative
   mych4$dydt <- get_dxdy(mych4$time, mych4$ch4smooth)
-
+  
   # compute density probability of first derivative
   d <- density(mych4$dydt)
-  half_dens_max <- max(d$y)/2
+  half_dens_max <- 0.5*max(d$y)
   ind_over_half_dens_max <- which(d$y>half_dens_max)
-
+  
   # we define lower and upper boundaries of the "main slope" as the slopes with
   # a density of probability higher than half of the maximum density
   lower_bound <- d$x[first(ind_over_half_dens_max)]
   upper_bound <- d$x[last(ind_over_half_dens_max)]
-
-  # plot(d)
-  # abline(h=half_dens_max, col='grey')
-  # abline(v=c(lower_bound, upper_bound), col='lightblue')
-
+  
+  # most_probable_slope <- d$x[which.max(d$y)]
+  
   avg_slope <- mean(d$x[ind_over_half_dens_max])
   sd_slope <- sd(d$x[ind_over_half_dens_max])
-
-  plot(mych4$time, mych4$ch4, type='l')
-  lines(mych4$time, mych4$ch4smooth, col='black')
-  points(mych4$time[mych4$dydt>lower_bound & mych4$dydt<upper_bound], mych4$ch4smooth[mych4$dydt>lower_bound & mych4$dydt<upper_bound], col = 'red')
-  abline(a = first(mych4$ch4smooth), b = avg_slope, col= 'red')
-  abline(a = first(mych4$ch4smooth), b = avg_slope+sd_slope/2, col= 'grey')
-  abline(a = first(mych4$ch4smooth), b = avg_slope-sd_slope/2, col= 'grey')
-
+  
+  
+  mych4_sel <- mych4[mych4$dydt>lower_bound & mych4$dydt<upper_bound,]
+  my_lm <- lm(data = mych4_sel, formula = ch4smooth~time)
+  my_c0 <- coefficients(my_lm)[1]
+  
+  p_density <- ggplot(mych4, aes(dydt))+
+    geom_rect(aes(xmin = lower_bound, ymin = -Inf, xmax = upper_bound, ymax = Inf), fill = "#FF7F50", alpha=0.1)+
+    geom_hline(yintercept = half_dens_max, alpha=0.5)+
+    geom_density()+
+    xlab("Rate of change of CH4 [ppb/secs]")+
+    theme_article()
+  
+  p_fit <- ggplot(mych4)+
+    geom_path(aes(time, ch4, colour = "raw"), linewidth=0.8)+
+    geom_path(aes(time, ch4smooth, colour = "smooth"), linewidth = 1)+
+    geom_point(data = mych4_sel, aes(time, ch4smooth, colour = "selected"), size=2, alpha=0.5)+
+    geom_abline(slope = avg_slope, intercept = my_c0, color = "red")+
+    geom_abline(slope = avg_slope+sd_slope/2, intercept = my_c0, color = "grey70")+
+    geom_abline(slope = avg_slope-sd_slope/2, intercept = my_c0, color = "grey70")+
+    xlab("time [secs]")+
+    ylab("CH4 [ppb]")+
+    scale_color_manual(values=c("#999999", "#E69F00", "#56B4E9"))+
+    ggtitle(UniqueID)+theme_article()+theme(legend.title = element_blank())
+  
+  ggarrange(p_density, p_fit, widths = c(0.3,0.7))
+  
   delta_C <- last(mych4$ch4smooth) - min(mych4$ch4smooth)
   duration <- last(mych4$time)
-
-
+  
+  
   df_out <- data.frame(duration = last(mych4$time),
                        delta_ch4 = last(mych4$ch4smooth) - min(mych4$ch4smooth),
                        avg_diff_slope = avg_slope,
                        sd_diff_slope = sd_slope)
-
+  
   return(df_out)
 }
 
