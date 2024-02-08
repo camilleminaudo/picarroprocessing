bd448468d3b9826357c63a0732db465414f2804d
camille.minaudo <camille.minaudo@ub.edu>
2024-02-08 17:35:59 +0100

    re-knit and updated list of packages

--- scripts/000_install_dependencies.R
+++ scripts/000_install_dependencies.R
@@ -8,7 +8,7 @@ if (!require("pacman", quietly = TRUE))
 # Then load packages. Pacman will check whether each package has been installed, and if not, will install it automatically.
 
 pacman::p_load("data.table", "dplyr",  "egg", "ggnewscale", "ggplot2", "graphics", "grDevices", "grid", "gridExtra",
-               "lubridate", "minpack.lm", "msm", "pbapply", "plyr", "purrr", "readxl", "remotes","rjson", "rlist", "SimDesign",
+               "lubridate", "minpack.lm", "msm", "pbapply", "plyr", "purrr", "readxl", "remotes","rmarkdown","rjson", "rlist", "SimDesign",
                "stats", "tibble", "tidyr", "tools","utils")
 
 remotes::install_github("Qepanna/GoFluxYourself")
